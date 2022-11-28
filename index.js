4d29121ea3254570c374d4db49c33684402dc4da
Surapat Mekvanich <titleyugio@gmail.com>
2022-11-28 08:09:27 -0800

    commit tree 4d2118d5b9616e4622d01094da15c3641e1820b0

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+
+function saturdayFun(activity) {
+    var activity = 'roller-skate'
+    return 'This Saturday, I want to ' + activity + '!'
+   
+}
+
+saturdayFun(activity)
+
+
+function mondayWork() {
+    goToOffice
+    return 'This Monday, I will work from home.'
+}
+
+mondayWork()
+
